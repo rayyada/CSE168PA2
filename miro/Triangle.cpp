fd5375cb74369c7090b103d0377e894564c8c511
Ray Yada <Ray Yada>
2016-04-19 00:28:52 -0700

    triangle intersection finished?

--- miro/Triangle.cpp
+++ miro/Triangle.cpp
@@ -45,9 +45,37 @@ Triangle::intersect(HitInfo& result, const Ray& r,float tMin, float tMax)
 	const Vector3 & n1 = m_mesh->normals()[ti3_n.y]; //normal b of triangle
 	const Vector3 & n2 = m_mesh->normals()[ti3_n.z]; //normal c of triangle
 
+	Vector3 rayOrigin = r.o;
+	Vector3 rayDir = r.d;
 	//Solve for normal of the plane. In this case, the triangle
 	Vector3 triangleNormal = (n0 + n1 + n2) / 3;
 	triangleNormal.normalize();
-	float D = (triangleNormal.x * v0.x) + (triangleNormal.y * v0.y) + (triangleNormal.z * v0.z);
-    return false;
+	float D = dot(triangleNormal, v0);
+
+	// Check if dot(n, d) == 0. If it's 0, the ray is parallel to the plane, and would give a divide by zero when caluclating t
+	float parallelCheck = dot(triangleNormal, rayDir);
+	if (parallelCheck == 0)
+	{
+		return false;
+	}
+
+	// P = o + tR
+	// Ax + By + Cz + D = 0
+	float t = -(dot(triangleNormal, rayOrigin) + D) / parallelCheck;
+    
+	// Calculate point where ray intersects plane. Note that this does not mean it intersects the triangle
+	Vector3 intersection = rayOrigin + (t*rayDir);
+
+	//Inside-outside test
+	Vector3 edge0 = v1 - v0;
+	Vector3 edge1 = v2 - v1;
+	Vector3 edge2 = v0 - v2;
+	Vector3 check0 = intersection - v0;
+	Vector3 check1 = intersection - v1;
+	Vector3 check2 = intersection - v2;
+	if (dot(triangleNormal, cross(edge0, check0)) > 0 && dot(triangleNormal, cross(edge1, check1)) > 0 && dot(triangleNormal, cross(edge2, check2)) > 0)
+	{
+		return true;
+	}
+	return false;
 }
